c7c3b76cb13ade1546ff1625969f49806837598d
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-22 13:03:59 +0300

    commit tree 2b3fd1feffc54ffb399ba9e77066cc8e870c2e59

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color,temperament, alive)
+VALUES('Mr. Chocolate', 4, 'M', 'White', 'high', true),
+      ('Rowdy', 7, 'M', 'Black', 'high', true),
+      ('Tabitha', 5, 'F', 'blue', 'low', false),
+      ('Sergeant Brown', 8, 'M', 'green', 'mid', true),
+      ('Melissa', 6, 'F', 'black', 'high', true),
+      ('Grinch', 3, 'M', 'white', 'high', false),
+      ('Wendy', 8, 'F', 'white', 'low', true),
+      (NULL, NULL, 'M', 'black', NULL, false);
